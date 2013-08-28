73d81a00d4052c2ddcb1afc2f625ebb40b168ff1
Jeroen <jeroenooms@gmail.com>
2013-08-29 01:10:52 +0200

    fix for osx corefoundation

--- R/rmdtext.R
+++ R/rmdtext.R
@@ -1,4 +1,6 @@
 rmdtextencoded <- function(text){
+  #fix for osx
+  options(bitmapType = 'cairo');
   writeLines(URLdecode(text), con="input.Rmd");
   knit2html("input.Rmd", output="output.html");
   invisible();
